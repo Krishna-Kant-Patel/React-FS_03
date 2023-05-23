381f4f1b5c3f31a0c5035827756bb8548304ae9d
Cool-Krish <61001421+Cool-Krish@users.noreply.github.com>
2023-05-23 22:24:52 +0530

    :fire: implement props

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,13 @@
+
+import './head.css'
+const Home = () =>{
+    return <>
+        <div className="header">
+            <div className="menu">Home</div>
+            <div className="menu">Show</div>
+            <div className="menu">About</div>
+            <div className="menu">Contact us</div>
+        </div>
+    </>
+}
+export default Home;
---
+++ my-app/src/components/card.js
@@ -0,0 +1,18 @@
+import './head.css'
+
+
+const Propcomp = (prop)  =>{
+
+    return <>
+        <div className="card">
+            <img src="" alt="" />
+            <br />
+            <h1>{prop.name}</h1>
+            <br />
+            <h1>Gender: {prop.gender}</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non assumenda libero cupiditate perspiciatis! Velit repellendus eligendi quis, consectetur delectus quos aut perferendis soluta ab ipsum veniam, ducimus ad accusamus animi!</p>
+        </div>
+    </>
+}
+export default Propcomp;
