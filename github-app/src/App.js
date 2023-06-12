53085ca9c7ca0f412a062aafee03280a997187fd
Cool-Krish <61001421+Cool-Krish@users.noreply.github.com>
2023-06-12 22:08:34 +0530

    :fire: To Do List

--- github-app/src/App.js
+++ github-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Home from './components/github/Home';
 
 function App() {
   return (
     <div className="App">
-      ch
+      <Home/>
     </div>
   );
 }
